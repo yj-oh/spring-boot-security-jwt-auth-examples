28739a726f97c255ca898df9752de9f5bf4fd2de
yj-oh <holidayoh99@gmail.com>
2021-12-14 00:56:50 +0900

    [추가] Main method

---
+++ src/main/java/com/yjworld/boilerplate/gradle/springboot/SpringbootGradleBoilerplateApplication.java
@@ -0,0 +1,12 @@
+package com.yjworld.boilerplate.gradle.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGradleBoilerplateApplication.class, args);
+    }
+}
