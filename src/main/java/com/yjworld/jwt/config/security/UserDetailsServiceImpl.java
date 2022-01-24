d22ed973fc6f36d6593e9d1af0a29ae5a7756aee
yj-oh <holidayoh99@gmail.com>
2022-01-24 20:41:13 +0900

    [추가] UserDetails, UserDetailsService 구현 클래스 생성

---
+++ src/main/java/com/yjworld/jwt/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.yjworld.jwt.config.security;
+
+import com.yjworld.jwt.domain.user.User;
+import com.yjworld.jwt.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException(username));
+
+		return UserDetailsImpl.build(user);
+	}
+}
