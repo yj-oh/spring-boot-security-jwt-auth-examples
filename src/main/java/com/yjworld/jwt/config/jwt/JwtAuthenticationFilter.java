3962021e41a99c9b733de4af41c049f8348ae155
yj-oh <holidayoh99@gmail.com>
2022-01-24 21:08:10 +0900

    [추가] 요청에 대한 인증 확인 위해 OncePerRequestFilter를 상속받는 필터 추가
    인증 확인은 요청 당 한 번만 실행하도록 하기 위해 OncePerRequestFilter 상속

---
+++ src/main/java/com/yjworld/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.yjworld.jwt.config.jwt;
+
+import com.yjworld.jwt.config.security.UserDetailsServiceImpl;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final UserDetailsServiceImpl userDetailsService;
+	private final JwtUtils jwtUtils;
+
+	public JwtAuthenticationFilter(UserDetailsServiceImpl userDetailsService, JwtUtils jwtUtils) {
+		this.userDetailsService = userDetailsService;
+		this.jwtUtils = jwtUtils;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain filterChain) throws ServletException, IOException {
+		String token = jwtUtils.getTokenFromHeader(request);
+
+		if (token != null && jwtUtils.isValidToken(token)) {
+			Claims claims = jwtUtils.getClaims(token);
+			String username = claims.get("username", String.class);
+
+			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+					userDetails, null, userDetails.getAuthorities());
+			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		} else {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+}
