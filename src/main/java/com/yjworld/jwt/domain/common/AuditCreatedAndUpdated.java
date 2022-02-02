02310b613ce0d6d786eadd7d5f81cdce3a939cad
yj-oh <holidayoh99@gmail.com>
2022-02-03 01:57:21 +0900

    [추가] AuditCreatedAndUpdated - 생성일시, 수정일시 공통 컬럼

---
+++ src/main/java/com/yjworld/jwt/domain/common/AuditCreatedAndUpdated.java
@@ -0,0 +1,23 @@
+package com.yjworld.jwt.domain.common;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditCreatedAndUpdated {
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
