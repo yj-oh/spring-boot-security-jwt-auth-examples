81a72c66e745c289dddbff989b7d284377114a43
yj-oh <holidayoh99@gmail.com>
2022-02-03 04:51:54 +0900

    [수정] 샘플 API | return값 변경 : "success" -> "Hello, {username}"

--- src/main/java/com/yjworld/jwt/domain/sample/SampleController.java
+++ src/main/java/com/yjworld/jwt/domain/sample/SampleController.java
@@ -1,17 +1,24 @@
 package com.yjworld.jwt.domain.sample;
 
+import com.yjworld.jwt.domain.auth.AuthService;
+import com.yjworld.jwt.domain.user.User;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/samples")
 public class SampleController {
+
+	private final AuthService authService;
 
 	@ApiOperation(value = "테스트용 샘플 API")
 	@GetMapping
 	public String test() {
-		return "success";
+		User user = authService.getCurrentUser();
+		return "Hello, " + user.getUsername();
 	}
 }
