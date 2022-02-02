2160adaf872a8a388d57a9bd48a056fdc3420d7e
yj-oh <holidayoh99@gmail.com>
2022-02-03 03:39:38 +0900

    [수정] 로그인 | response에 토큰 타입 필드 추가

--- src/main/java/com/yjworld/jwt/domain/auth/JwtResponseDto.java
+++ src/main/java/com/yjworld/jwt/domain/auth/JwtResponseDto.java
@@ -1,5 +1,6 @@
 package com.yjworld.jwt.domain.auth;
 
+import com.yjworld.jwt.config.jwt.JwtUtils;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +21,9 @@ public class JwtResponseDto {
 
 	@ApiModelProperty(value = "사용자 이름", required = true)
 	private String username;
+
+	@ApiModelProperty(value = "토큰 타입", required = true)
+	private final String tokenType = JwtUtils.TOKEN_TYPE;
 
 	@ApiModelProperty(value = "토큰", required = true)
 	private String token;
